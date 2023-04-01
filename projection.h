a915afd0f1a7e8f41d5ee4c7fbfb1c542b1e1145
abatsis <abatsis@mail.com>
2023-04-01 18:27:08 +0100

    Add projection class

---
+++ projection.h
@@ -0,0 +1,22 @@
+#pragma
+#include <vector>
+
+class Projection
+{
+    float parameter_;
+
+public:
+    Projection(float parameter) : parameter_(parameter){};
+
+    float operator()(std::vector<float> integers)
+    {
+        float powerOfParameter = 1;
+        float result = 0;
+        for (auto const &integer : integers)
+        {
+            result = result + integer * powerOfParameter;
+            powerOfParameter = powerOfParameter * parameter_;
+        }
+        return result;
+    }
+};
